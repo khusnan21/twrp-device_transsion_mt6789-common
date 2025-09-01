dcbebd1a302e9c50f19aeb5666cc766676307b47
khusnan21 <khusnan21@gmail.com>
2025-09-02 00:03:03 +0700

    Update common.mk

--- common.mk
+++ common.mk
@@ -114,8 +114,7 @@ TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
 TARGET_RECOVERY_DEVICE_MODULES += \
     android.hardware.keymaster@4.1
 
-# Force permissive SELinux for recovery
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_VENDOR_PROPERTIES += \
     ro.build.selinux=0 \
     selinux.reload_policy=0
 
