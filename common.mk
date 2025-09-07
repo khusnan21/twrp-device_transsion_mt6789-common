9b409024fb2a0dd7ee4039757b45223791f45aa9
khusnan21 <khusnan21@gmail.com>
2025-09-07 17:21:40 +0700

    Update common.mk

--- common.mk
+++ common.mk
@@ -136,5 +136,6 @@ RECOVERY_BINARY_SOURCE_FILES += $(TARGET_OUT_EXECUTABLES)/snapuserd
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
-TARGET_OTA_CERTIFICATE := device/itel/P1102GT/security/releasekey.x509.pem
-PRODUCT_EXTRA_RECOVERY_KEYS += device/itel/P1102GT/security/releasekey
+# Otacert
+PRODUCT_EXTRA_RECOVERY_KEYS += \
+    $(LOCAL_PATH)/security/releasekey
